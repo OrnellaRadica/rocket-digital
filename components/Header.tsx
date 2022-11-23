bd325bc4b0a0cc6c138b5f84a577baf180fe05b1
Ornella Radica <radicaornella@gmail.com>
2022-11-23 17:55:38 +0100

    Fix header style

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,7 +2,7 @@ import { Logo } from "./Icons";
 
 export function Header() {
   return (
-    <header className="py-4">
+    <header className="py-4 max-w-[1500px] mx-auto px-6 lg:px-20">
       <div className="sm:w-[120px] sm:h-[54px] w-[72px] h-[32px] flex items-center">
         <Logo />
       </div>
